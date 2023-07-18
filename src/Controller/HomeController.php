aee15152dfcfe4651d5ce9bd568d7675e29b910c
Noemie.BARRE <noemie.barre24@gmail.com>
2023-07-18 11:50:41 +0200

    add error page without the style and some home page fonctionality like being able to see the most like music

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MusicRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
-        return $this->render('Home/index.html.twig');
+    public function index(MusicRepository $musicRepository): Response
+    { 
+        $musics = $musicRepository->findFavoriteMusic();
+        return $this->render('Home/index.html.twig',[
+            'musics' => $musics,
+    ]);
     }
 }
