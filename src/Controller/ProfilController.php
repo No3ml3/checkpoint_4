8208c456a7857bc67cf28ac8a55ae6597a3928a6
Noemie.BARRE <noemie.barre24@gmail.com>
2023-07-19 16:13:47 +0200

    work some stuff out

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\ProfilEditType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class ProfilController extends AbstractController
 {
@@ -14,6 +18,25 @@ class ProfilController extends AbstractController
     {
         return $this->render('profil/index.html.twig', [
             'user' => $user,
+        ]);
+    }
+    #[Route('/profil/{id}/modifie', name: 'app_profil_modifie')]
+    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ProfilEditType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            /** @var \App\Entity\User */
+            $user = $this->getUser();
+            return $this->redirectToRoute('app_profil', ['id' => $user->getId()], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('profil/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
         ]);
     }
 }
